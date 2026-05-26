1798f3b051875556cd43e6d47c9c35899d3328c6
luoms <xiongze666@gmail.com>
2026-05-26 22:42:38 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -9,12 +9,12 @@ VOICES_DIR = BASE_DIR / "voices"
 OUTPUT_DIR = BASE_DIR / "output"
 PERSONA_PATH = BASE_DIR / "persona.txt"
 
-OLLAMA_BASE_URL = "http://localhost:11434"
-OLLAMA_MODEL = "qwen3-coder:480b-cloud"
+OLLAMA_BASE_URL = "http://localhost:11434"#修改为你自己的 Ollama 服务器地址
+OLLAMA_MODEL = "qwen3-coder:480b-cloud"#修改为你自己的模型名称
 
 GPT_SOVITS_GPT_WEIGHTS = Path(
-	"E:/gpt-sovits/GPT-SoVITS-v2pro-20250604/GPT_weights_v2Pro/www-e10.ckpt"
+	"E:/gpt-sovits/GPT-SoVITS-v2pro-20250604/GPT_weights_v2Pro/www-e10.ckpt"#修改为你自己的权重路径 
 )
 GPT_SOVITS_SOVITS_WEIGHTS = Path(
-	"E:/gpt-sovits/GPT-SoVITS-v2pro-20250604/SoVITS_weights_v2Pro/www_e4_s176.pth"
+	"E:/gpt-sovits/GPT-SoVITS-v2pro-20250604/SoVITS_weights_v2Pro/www_e4_s176.pth" #修改为你自己的权重路径  
 )
